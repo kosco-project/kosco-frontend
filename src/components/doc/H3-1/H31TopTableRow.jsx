53d82084b23fc151fe66b57281c37888a628943a
youngseo-im <gg2922914@gmail.com>
2021-03-16 01:34:07 +0900

    feat : H3-1 컴포넌트 리스트 추가, 제거 (커스텀 훅 사용)

--- src/components/doc/H3-1/H31TopTableRow.jsx
+++ src/components/doc/H3-1/H31TopTableRow.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { GoX } from 'react-icons/go'; 
+import styled from 'styled-components';
 
-const H31TopTableRow = () => {
+const DeleteButton = styled.button`
+  cursor: pointer;
+  border: none;
+  width: 100%;
+  padding-top: 13px;
+  padding-bottom: 13px;
+`;
+
+const H31TopTableRow = ({ id, onRemove, num }) => {
   return (
     <div>
       <div className='title'>
-        <input type='text' style={{ width: '100%' }} />
+      {'SET NO.' + num}
       </div>
       <div className='description'>
         <input type='checkbox' defaultChecked='checked' />
@@ -26,6 +36,11 @@ const H31TopTableRow = () => {
       </div>
       <div className='description'>
         <input type='checkbox' defaultChecked='checked' />
+      </div>
+      <div>
+       <DeleteButton type="button" onClick={() => onRemove(id)}>
+          <GoX fill="#e92a2a" size="18px"/>
+        </DeleteButton>
       </div>
     </div>
   );
