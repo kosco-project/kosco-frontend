91e010a585323ccc6cc12a0a6c6a1c0be5f70499
youngseo-im <gg2922914@gmail.com>
2021-03-16 01:23:15 +0900

    feat : H2-A 컴포넌트 리스트 추가, 제거 (커스텀 훅 사용)

--- src/components/doc/H2-A/H2ATableRow.jsx
+++ src/components/doc/H2-A/H2ATableRow.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import { GoX } from 'react-icons/go'; 
 
-const H2ATableRow = () => {
+
+const DeleteButton = styled.button`
+  cursor: pointer;
+  border: none;
+  width: 100%;
+  padding-top: 13px;
+  padding-bottom: 13px;
+`;
+
+const H2ATableRow = ({ id, onRemove }) => {
   return (
     <tr>
       <td>
@@ -20,6 +31,11 @@ const H2ATableRow = () => {
       </td>
       <td>
         <input type='text' />
+      </td>
+      <td>
+        <DeleteButton type="button" onClick={() => onRemove(id)}>
+          <GoX fill="#e92a2a" size="18px"/>
+        </DeleteButton>
       </td>
     </tr>
   );
