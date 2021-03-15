bccfbf87b684b12e84e0e8fab3d8555fe149d39a
youngseo-im <gg2922914@gmail.com>
2021-03-15 23:27:34 +0900

    feat : I 컴포넌트 리스트 추가, 제거 커스텀 훅으로 분리

--- src/components/doc/I/ITableRow.jsx
+++ src/components/doc/I/ITableRow.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import { GoX } from 'react-icons/go'; 
 
-const ITableRow = ({ num }) => {
+
+const DeleteButton = styled.button`
+  cursor: pointer;
+  border: none;
+  width: 100%;
+  padding-top: 13px;
+  padding-bottom: 13px;
+`;
+
+const ITableRow = ({ id, onRemove, num }) => {
   return (
     <tr>
       <td>{num}</td>
@@ -12,6 +23,11 @@ const ITableRow = ({ num }) => {
       </td>
       <td>
         <input type='date' />
+      </td>
+      <td>
+        <DeleteButton type="button" onClick={() => onRemove(id)}>
+          <GoX fill="#e92a2a" size="18px"/>
+        </DeleteButton>
       </td>
     </tr>
   );
