296944e2b9b785f6651dcb23f92d82834d1085ac
youngseo-im <gg2922914@gmail.com>
2021-03-23 11:28:53 +0900

    etc: 주석 삭제

--- src/containers/doc/A1Container.js
+++ src/containers/doc/A1Container.js
@@ -1,5 +1,3 @@
-// import axios from 'axios';
-// import moment from 'moment';
 import React from 'react';
 import A1Form from "../../components/doc/A1/A1Form";
 import useTemporaryStorage from '../../hooks/useTemporaryStorage';
