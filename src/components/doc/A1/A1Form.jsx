a506fc1244c4ddbfa2157ae54e2283ab18a40aed
youngseo-im <gg2922914@gmail.com>
2021-03-31 15:58:39 +0900

    feat: A1 컴포넌트 검사 완료 기능 구현

--- src/components/doc/A1/A1Form.jsx
+++ src/components/doc/A1/A1Form.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import A1Bottom from './A1Bottom';
 import A1Top from './A1Top';
-import ButtonBox from '../common/ButtonBox';
+import TemporaryStorageButton from '../common/TemporaryStorageButton';
+import InspectionCompleteButton from '../common/InspectionCompleteButton';
 import Info from '../common/Info';
 
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 100px;
+  text-align: center;
+  Button {
+    margin: 0 15px;
+  }`;
 const A1Form = ({ onProductsDescription, onInspectionDescription, temporaryStorage }) => {
 
   return (
-    <form onSubmit={e => temporaryStorage(e, {form : "A1"})}>
+    <form>
       <Info />
       <A1Top onProductsDescription={onProductsDescription}/>
-      <A1Bottom onInspectionDescription={onInspectionDescription}/>
-      <ButtonBox />
+      <A1Bottom onInspectionDescription={onInspectionDescription} />
+      <ButtonBox>
+        <TemporaryStorageButton temporaryStorage={temporaryStorage} form="A1" path="save"/>
+        <InspectionCompleteButton temporaryStorage={temporaryStorage} form="A1" path="complete"/>
+      </ButtonBox>
     </form>
   );
 };
