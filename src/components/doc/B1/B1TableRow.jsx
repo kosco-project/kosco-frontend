dd08c946738e3d526b0c403a2a70bdd2709c0be0
youngseo-im <gg2922914@gmail.com>
2021-04-07 14:47:02 +0900

    style: 코드 수정

--- src/components/doc/B1/B1TableRow.jsx
+++ src/components/doc/B1/B1TableRow.jsx
@@ -5,42 +5,44 @@ import { useSelector } from 'react-redux';
 import DeleteButton from '../common/DeleteButton';
 
 const TableRow = ({ id, onRemove, onChange, num }) => {
-  const  { TestDt }  = useSelector(state => state.b1.D1[id]);
+  const { TestDt } = useSelector(state => state.b1.D1[id]);
+  const inputArg = ({ target }) => onChange({ target, id });
+
   return (
     <tr>
       <td>{num}</td>
       <td>
-        <input type='text' onChange={({ target }) => onChange({ target, id })} name="GasType"/>
+        <input type='text' onChange={inputArg} name="GasType"/>
       </td>
       <td>
-        <input type='text' onChange={({ target }) => onChange({ target, id })} name="SerialNo"/>
+        <input type='text' onChange={inputArg} name="SerialNo"/>
       </td>
       <td>
         <DatePicker
           selected={TestDt}
           name="TestDt"
-          dateFormat="MMM.yy"
+          dateFormat="MMM.yyyy"
           onChange={value => onChange({ id, target: { name: "TestDt", value } })}
           showMonthYearPicker
         />
       </td>
       <td>
-        <input type='text' onChange={({ target }) => onChange({ target, id })} name="TareWT" />
+        <input type='text' onChange={inputArg} name="TareWT" />
       </td>
       <td>
-        <input type='text' onChange={({ target }) => onChange({ target, id })} name="GrossWT" />
+        <input type='text' onChange={inputArg} name="GrossWT" />
       </td>
       <td>
-        <input type='text' onChange={({ target }) => onChange({ target, id })} name="Capacity" />
+        <input type='text' onChange={inputArg} name="Capacity" />
       </td>
       <td>
-        <input type='text' onChange={({ target }) => onChange({ target, id })}  name="Press" />
+        <input type='text' onChange={inputArg}  name="Press" />
       </td>
       <td>
-        <input type='text' onChange={({ target }) => onChange({ target, id })} name="Temp" />
+        <input type='text' onChange={inputArg} name="Temp" />
       </td>
       <td>
-        <select onChange={({ target }) => onChange({ target, id })} name="Perform" >
+        <select onChange={inputArg} name="Perform" >
           <option value='GOOD'>GOOD</option>
           <option value='BAD'>BAD</option>
         </select>
