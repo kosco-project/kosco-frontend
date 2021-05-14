f20c4b03dddbf49810f3aca7866a1f6070a1ff0e
Kim-Jaemin420 <jany240@naver.com>
2021-05-14 17:02:37 +0900

    fix: perform 초기 상태 값 설정

--- src/redux/modules/h2A.js
+++ src/redux/modules/h2A.js
@@ -33,7 +33,7 @@ const initialState = {
       WorkPress: '',
       SerialNo: '',
       TestDt: new Date(),
-      Perform: 'GOOD',
+      Perform: '선택해주세요.',
     },
     1: {
       CylnType: '',
@@ -41,7 +41,7 @@ const initialState = {
       WorkPress: '',
       SerialNo: '',
       TestDt: new Date(),
-      Perform: 'GOOD',
+      Perform: '선택해주세요.',
     },
     2: {
       CylnType: '',
@@ -49,7 +49,7 @@ const initialState = {
       WorkPress: '',
       SerialNo: '',
       TestDt: new Date(),
-      Perform: 'GOOD',
+      Perform: '선택해주세요.',
     },
     3: {
       CylnType: '',
@@ -57,7 +57,7 @@ const initialState = {
       WorkPress: '',
       SerialNo: '',
       TestDt: new Date(),
-      Perform: 'GOOD',
+      Perform: '선택해주세요.',
     },
   },
   D2: '',
@@ -97,7 +97,7 @@ const H2AReducer = handleActions(
             WorkPress: '',
             SerialNo: '',
             TestDt: new Date(),
-            Perform: '',
+            Perform: '선택해주세요.',
           },
         },
       };
