babe6ccfc8c1a746ee2b9c89915a1bd2d78cd67d
Kim-Jaemin420 <jany240@naver.com>
2021-05-21 14:01:42 +0900

    feat: 페이지 이동시 스크롤을 상단으로 초기화

---
+++ src/hooks/ScrollToTop.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
+const ScrollToTop = ({ children, location: { pathname } }) => {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, [pathname]);
+
+  return children || null;
+};
+
+export default withRouter(ScrollToTop);
