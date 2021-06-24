5b1305ecdaa0b8a507d9472ed91a8e0f57d722c1
Kim-Jaemin420 <jany240@naver.com>
2021-06-24 20:15:38 +0900

    feat: 검색 입력 폼 레이아웃 css 변경

--- src/components/inspection/InspectionBox.jsx
+++ src/components/inspection/InspectionBox.jsx
@@ -1,16 +1,30 @@
 import React, { memo } from 'react';
+import styled from 'styled-components';
 import InspectionCondition from './InspectionCondition';
 import InspectionList from './InspectionList';
 import InspectionTitleBox from './InspectionTitleBox';
+import InputSearch from '../common/InputSearch';
 
-const InspectionBox = ({ list, pageList, onChangeDate, onChangeProcess, inputValue, onChangePage }) => {
+const ConditionWrapper = styled.div`
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+  padding: 0 20px;
+`;
+
+const InspectionBox = ({ list, setList, pageList, setPageList, onChangeDate, onChangeProcess, inputValue, onChangePage }) => {
 
   return (
+    <>
+    <ConditionWrapper>
+      <InspectionCondition  inputValue={inputValue} onChangeDate={onChangeDate} onChangeProcess={onChangeProcess} />
+      <InputSearch list={list} setList={setList} setPageList={setPageList} />
+    </ConditionWrapper>
     <div>
-      <InspectionCondition inputValue={inputValue} onChangeDate={onChangeDate} onChangeProcess={onChangeProcess} />
       <InspectionTitleBox />
       <InspectionList inputValue={inputValue} list={list} pageList={pageList} onChangePage={onChangePage} />
     </div>
+    </>
   );
 };
 
