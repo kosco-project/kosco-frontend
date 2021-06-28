22ea12d8b1674d37e546f9dcdc2bc99bc345418d
Kim-Jaemin420 <jany240@naver.com>
2021-06-28 15:44:52 +0900

    feat: 검색창 레이아웃 및 상태 설정

--- src/components/common/InputSearch.jsx
+++ src/components/common/InputSearch.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import { Select, Input, Space } from 'antd';
-import { AudioOutlined } from '@ant-design/icons';
-
+import React, { useCallback, useState } from 'react';
+import { Select, Input } from 'antd';
 import styled from 'styled-components';
+
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import { getInspectionList } from '../../redux/modules/inspectionList';
 
 const { Option } = Select;
 const { Search } = Input;
@@ -19,31 +21,47 @@ const Form = styled.form`
   margin-top: 55px;
 `;
 
-const InputSearch = ({list, setList}) => {
-  const [searchType, setSearchType] = useState('custNm');
+const InputSearch = () => {
+  const dispatch = useDispatch();
 
-  const handleChange = name => {
+  const [searchType, setSearchType] = useState('CUSTNM');
+
+  const handleChange = useCallback(name => {
     setSearchType(name);
+  }, []);
+
+  const onSearch = async value => {
+    try {
+      const res = await axios.get(
+        `${
+          process.env.REACT_APP_SERVER_URL
+        }/api/inspectionList/${sessionStorage.getItem('startDate')}/${sessionStorage.getItem('endDate')}/${sessionStorage.getItem('process') || 1}?sch_type=${searchType}&sch_keyword=${value}`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('KOSCO_token')}`,
+          },
+        }
+      );
+
+      dispatch(getInspectionList(res.data.list_info))
+    } catch (e) {
+      console.log(e.res);
+      console.log(e);
+    }
+
   };
 
-  const onSearch = value => {
-    console.log(value);
-  };
-
-  const onchange = ({target}) => {
-    console.log(target.value);
-  }
   return (
     <Form>
-    <SelectWrapper defaultValue="custNm" onChange={handleChange}>
-      <Option value="custNm">고객명</Option>
-      <Option value="rcvDt">수주일자</Option>
-      <Option value="shipNm">선박명</Option>
-      <Option value="rcvNo">수주번호</Option>
-      <Option value="docNo">양식번호</Option>
-      <Option value="certNo">서트관리번호</Option>
+    <SelectWrapper defaultValue="CUSTNM" onChange={handleChange}>
+      <Option value="CUSTNM">고객명</Option>
+      <Option value="RCVDT">수주일자</Option>
+      <Option value="SHIPNM">선박명</Option>
+      <Option value="RCVNO">수주번호</Option>
+      <Option value="DOC_NO">양식번호</Option>
+      <Option value="CERT_NO">서트관리번호</Option>
     </SelectWrapper>
-     <SearchWrapper placeholder="검색어를 입력해주세요" allowClear onChange={onchange} onSearch={onSearch} />
+     <SearchWrapper placeholder="검색어를 입력해주세요" allowClear onSearch={onSearch} />
     </Form>
   );
 };
