51d084ca7c285e2a0737e3f7d0a4f2a1ec8a7fa1
Kim-Jaemin420 <jany240@naver.com>
2021-06-30 21:40:27 +0900

    feat: 토큰 만료시 로그인 페이지로 이동

--- src/components/doc/A2/A2Form.jsx
+++ src/components/doc/A2/A2Form.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import A2Bottom from './A2Bottom';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { deleteInitialState, initialize } from '../../../redux/modules/docsInput';
+
+import useGetFetch from '../../../hooks/useGetFetch';
+
 import Info from '../common/Info';
 import A2Top from './A2Top';
+import A2Bottom from './A2Bottom';
 import TemporaryStorageButton from '../common/TemporaryStorageButton';
 import InspectionCompleteButton from '../common/InspectionCompleteButton';
+
+import A2_INIT from '../../../docsInitialState/A2';
 
 const ButtonBox = styled.div`
   display: flex;
@@ -15,15 +23,30 @@ const ButtonBox = styled.div`
     margin: 0 15px;
   }`;
 
-const A2Form = ({ onProductsDescription, onInspectionDescription, showModal, showCommModal, state }) => {
+const A2Form = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const state = useGetFetch(A2_INIT);
+
+  useEffect(() => {
+    if (!sessionStorage.getItem('KOSCO_token')) history.push('/');
+    dispatch(initialize(state));
+
+    return () => {
+      dispatch(deleteInitialState());
+    }
+  }, [dispatch, history, state]);
+
+  
+
   return (
     <form >
       <Info />
-      <A2Top onProductsDescription={onProductsDescription} state={state} />
-      <A2Bottom onInspectionDescription={onInspectionDescription} state={state} />
+      <A2Top />
+      <A2Bottom />
       <ButtonBox>
-        <TemporaryStorageButton showModal={showModal}/>
-        <InspectionCompleteButton showCommModal={showCommModal}/>
+        <TemporaryStorageButton />
+        <InspectionCompleteButton />
       </ButtonBox>
     </form>
   );
