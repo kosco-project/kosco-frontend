3a1ce9c388a7fed7064d586e76a0acaaa6e6c309
Kim-Jaemin420 <jany240@naver.com>
2021-06-30 18:51:23 +0900

    feat: data를 GET 하는 로직을 커스텀 훅으로 생성

---
+++ src/hooks/useGetFetch.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { useCallback, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const useGetFetch = (initialState = {}) => {
+  const [state, setState] = useState(initialState);
+
+  const location = useLocation();
+
+  const path = location.pathname.split('/')[2];
+  const CERTNO = JSON.parse(localStorage.getItem('certNo'));
+  const RCVNO = JSON.parse(localStorage.getItem('rcvNo'));
+  const VESSELNM = JSON.parse(localStorage.getItem('shipNm')) || '';
+
+  const getFetch = useCallback(async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_SERVER_URL}/api/doc/${path}?ct=${CERTNO}`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('KOSCO_token')}`,
+          },
+        }
+      );
+
+      const { D1, ...rest } = await res.data;
+      console.log('res', res.data);
+      if (!Object.keys(D1).length && !Object.keys(rest.D2).length) return;
+
+      await setState(() => ({
+        H: {
+          CERTNO,
+          RCVNO,
+          VESSELNM,
+        },
+        D1,
+        ...rest,
+      }));
+    } catch (e) {
+      if (e.response.status === 401 || e.response.status === 409) {
+        sessionStorage.removeItem('startDate');
+        sessionStorage.removeItem('endDate');
+        sessionStorage.removeItem('KOSCO_token');
+        window.location.replace('/');
+      }
+      console.log(e);
+    }
+  }, [CERTNO, RCVNO, VESSELNM, path]);
+
+  useEffect(() => {
+    getFetch();
+  }, [getFetch]);
+
+  return state;
+};
+export default useGetFetch;
