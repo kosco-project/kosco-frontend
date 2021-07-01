2abe42a130a6be3de4cd37366720b3be1143e023
Kim-Jaemin420 <jany240@naver.com>
2021-07-01 16:53:43 +0900

    fix: A3 문서 커스텀 훅과 공통 리듀서를 사용하도록 수정

---
+++ src/docsInitialState/A3.js
@@ -0,0 +1,141 @@
+const A3_INIT = {
+  H: {
+    CERTNO: '',
+    RCVNO: '',
+    VESSELNM: '',
+  },
+  D1: {
+    0: {
+      Value: '',
+      Unit: '대',
+      Remark: '',
+    },
+    1: {
+      Value: '',
+      Unit: '대',
+      Remark: '',
+    },
+    2: {
+      Value: '',
+      Unit: '대',
+      Remark: '',
+    },
+    3: {
+      Value: '',
+      Unit: '대',
+      Remark: '',
+    },
+    4: {
+      Value: '',
+      Unit: '대',
+      Remark: '',
+    },
+    5: {
+      Value: '',
+      Unit: '대',
+      Remark: '',
+    },
+    6: {
+      Value: '',
+      Unit: '대',
+      Remark: '',
+    },
+    7: {
+      Value: '',
+      Unit: '대',
+      Remark: '',
+    },
+  },
+  D2: {
+    0: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    1: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    2: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    3: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    4: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    5: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    6: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    7: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    8: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    9: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    10: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    11: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    12: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    13: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    14: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    15: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+    16: {
+      CarriedOut: 0,
+    },
+    17: {
+      CarriedOut: 0,
+      NotCarried: 0,
+      Remark: '',
+    },
+  },
+};
+
+export default A3_INIT;
