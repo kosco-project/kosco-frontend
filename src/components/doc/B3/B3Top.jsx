aeb2d34cdf3b8216cc2d26fc27d5d0f13452fee6
Kim-Jaemin420 <jany240@naver.com>
2021-07-02 20:11:38 +0900

    fix: B3 문서를 커스텀 훅과 공통 리듀서를 사용하도록 수정

--- src/components/doc/B3/B3Top.jsx
+++ src/components/doc/B3/B3Top.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import useChangeD1 from '../../../hooks/useChangeD1';
+import useGetUnits from '../../../hooks/useGetUnits';
 
 const TopBox = styled.div`
   margin-bottom: 15px;
@@ -53,147 +56,154 @@ const TopBox = styled.div`
   }
 `;
 
-const B3Top = ({ units, onWorkingSystem, state }) => {
-  const { D1 } = state;
+const B3Top = () => {
+  const D1 = useSelector(state => state.docsInput.D1);
+  const { units } = useGetUnits();
+
+  const onChangeD1 = useChangeD1();
 
   return (
-    <TopBox>
-      <p className='top-title'>Description of Working System</p>
-      <div className='top-description'>
-        <table>
-          <thead>
-            <tr>
-              <td>No.</td>
-              <td>Description</td>
-              <td>Value</td>
-              <td>Unit</td>
-              <td>Remarks</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Manufacturer</td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="0" data-key="Value" value={D1[0].Value || ''} onChange={onWorkingSystem}/>
-              </td>
-              <td>
-              <select name='' id='' data-form="D1" data-name="0" data-key="Unit" value={D1[0].Unit || ''} onChange={onWorkingSystem}>
-                  {units.map(({CdNm}) => (
-                    <option key={CdNm} value={CdNm}>
-                      {CdNm}
-                    </option>
-                  ))}
-                </select>
-              </td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="0" data-key="Remark" value={D1[0].Remark || ''} onChange={onWorkingSystem} />
-              </td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Model</td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="1" data-key="Value" value={D1[1].Value || ''} onChange={onWorkingSystem}/>
-              </td>
-              <td>
-              <select name='' id='' data-form="D1" data-name="1" data-key="Unit" value={D1[1].Unit || ''} onChange={onWorkingSystem}>
-                  {units.map(({CdNm}) => (
-                    <option key={CdNm} value={CdNm}>
-                      {CdNm}
-                    </option>
-                  ))}
-                </select>
-              </td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="1" data-key="Remark" value={D1[1].Remark || ''} onChange={onWorkingSystem} />
-              </td>
-            </tr>
-
-            <tr>
-              <td>3</td>
-              <td>Serial no.</td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="2" data-key="Value" value={D1[2].Value || ''} onChange={onWorkingSystem}/>
-              </td>
-              <td>
-              <select name='' id='' data-form="D1" data-name="2" data-key="Unit" value={D1[2].Unit || ''} onChange={onWorkingSystem}>
-                  {units.map(({CdNm}) => (
-                    <option key={CdNm} value={CdNm}>
-                      {CdNm}
-                    </option>
-                  ))}
-                </select>
-              </td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="2" data-key="Remark" value={D1[2].Remark || ''} onChange={onWorkingSystem} />
-              </td>
-            </tr>
-
-            <tr>
-              <td>4</td>
-              <td>Maximum Charging pressure</td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="3" data-key="Value" value={D1[3].Value || ''} onChange={onWorkingSystem}/>
-              </td>
-              <td>
-              <select name='' id='' data-form="D1" data-name="3" data-key="Unit" value={D1[3].Unit || ''} onChange={onWorkingSystem}>
-                  {units.map(({CdNm}) => (
-                    <option key={CdNm} value={CdNm}>
-                      {CdNm}
-                    </option>
-                  ))}
-                </select>
-              </td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="3" data-key="Remark" value={D1[3].Remark || ''} onChange={onWorkingSystem} />
-              </td>
-            </tr>
-
-            <tr>
-              <td>5</td>
-              <td>Revolution</td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="4" data-key="Value" value={D1[4].Value || ''} onChange={onWorkingSystem}/>
-              </td>
-              <td>
-              <select name='' id='' data-form="D1" data-name="4" data-key="Unit" value={D1[4].Unit || ''} onChange={onWorkingSystem}>
-                  {units.map(({CdNm}) => (
-                    <option key={CdNm} value={CdNm}>
-                      {CdNm}
-                    </option>
-                  ))}
-                </select>
-              </td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="4" data-key="Remark" value={D1[4].Remark || ''} onChange={onWorkingSystem} />
-              </td>
-            </tr>
-
-            <tr>
-              <td>6</td>
-              <td>Location of compressor</td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="5" data-key="Value" value={D1[5].Value || ''} onChange={onWorkingSystem}/>
-              </td>
-              <td>
-              <select name='' id='' data-form="D1" data-name="5" data-key="Unit" value={D1[5].Unit || ''} onChange={onWorkingSystem}>
-                  {units.map(({CdNm}) => (
-                    <option key={CdNm} value={CdNm}>
-                      {CdNm}
-                    </option>
-                  ))}
-                </select>
-              </td>
-              <td style={{ padding: '0 5px' }}>
-              <input type='text' data-form="D1" data-name="5" data-key="Remark" value={D1[5].Remark || ''} onChange={onWorkingSystem} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </TopBox>
+    <>
+      {D1 && (
+        <TopBox>
+        <p className='top-title'>Description of Working System</p>
+        <div className='top-description'>
+          <table>
+            <thead>
+              <tr>
+                <td>No.</td>
+                <td>Description</td>
+                <td>Value</td>
+                <td>Unit</td>
+                <td>Remarks</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Manufacturer</td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="0" data-key="Value" value={D1[0].Value} onChange={e => onChangeD1(e)}/>
+                </td>
+                <td>
+                <select name='' id='' data-form="D1" data-name="0" data-key="Unit" value={D1[0].Unit} onChange={e => onChangeD1(e)}>
+                    {units.map(({CdNm}) => (
+                      <option key={CdNm} value={CdNm}>
+                        {CdNm}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="0" data-key="Remark" value={D1[0].Remark} onChange={e => onChangeD1(e)} />
+                </td>
+              </tr>
+  
+              <tr>
+                <td>2</td>
+                <td>Model</td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="1" data-key="Value" value={D1[1].Value} onChange={e => onChangeD1(e)}/>
+                </td>
+                <td>
+                <select name='' id='' data-form="D1" data-name="1" data-key="Unit" value={D1[1].Unit} onChange={e => onChangeD1(e)}>
+                    {units.map(({CdNm}) => (
+                      <option key={CdNm} value={CdNm}>
+                        {CdNm}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="1" data-key="Remark" value={D1[1].Remark} onChange={e => onChangeD1(e)} />
+                </td>
+              </tr>
+  
+              <tr>
+                <td>3</td>
+                <td>Serial no.</td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="2" data-key="Value" value={D1[2].Value} onChange={e => onChangeD1(e)}/>
+                </td>
+                <td>
+                <select name='' id='' data-form="D1" data-name="2" data-key="Unit" value={D1[2].Unit} onChange={e => onChangeD1(e)}>
+                    {units.map(({CdNm}) => (
+                      <option key={CdNm} value={CdNm}>
+                        {CdNm}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="2" data-key="Remark" value={D1[2].Remark} onChange={e => onChangeD1(e)} />
+                </td>
+              </tr>
+  
+              <tr>
+                <td>4</td>
+                <td>Maximum Charging pressure</td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="3" data-key="Value" value={D1[3].Value} onChange={e => onChangeD1(e)}/>
+                </td>
+                <td>
+                <select name='' id='' data-form="D1" data-name="3" data-key="Unit" value={D1[3].Unit} onChange={e => onChangeD1(e)}>
+                    {units.map(({CdNm}) => (
+                      <option key={CdNm} value={CdNm}>
+                        {CdNm}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="3" data-key="Remark" value={D1[3].Remark} onChange={e => onChangeD1(e)} />
+                </td>
+              </tr>
+  
+              <tr>
+                <td>5</td>
+                <td>Revolution</td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="4" data-key="Value" value={D1[4].Value} onChange={e => onChangeD1(e)}/>
+                </td>
+                <td>
+                <select name='' id='' data-form="D1" data-name="4" data-key="Unit" value={D1[4].Unit} onChange={e => onChangeD1(e)}>
+                    {units.map(({CdNm}) => (
+                      <option key={CdNm} value={CdNm}>
+                        {CdNm}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="4" data-key="Remark" value={D1[4].Remark} onChange={e => onChangeD1(e)} />
+                </td>
+              </tr>
+  
+              <tr>
+                <td>6</td>
+                <td>Location of compressor</td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="5" data-key="Value" value={D1[5].Value} onChange={e => onChangeD1(e)}/>
+                </td>
+                <td>
+                <select name='' id='' data-form="D1" data-name="5" data-key="Unit" value={D1[5].Unit} onChange={e => onChangeD1(e)}>
+                    {units.map(({CdNm}) => (
+                      <option key={CdNm} value={CdNm}>
+                        {CdNm}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+                <td style={{ padding: '0 5px' }}>
+                <input type='text' data-form="D1" data-name="5" data-key="Remark" value={D1[5].Remark} onChange={e => onChangeD1(e)} />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </TopBox>
+      )}
+    </>
   );
 };
 
