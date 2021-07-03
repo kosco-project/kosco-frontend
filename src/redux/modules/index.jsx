45dea690ee5b401610442942a757d31558d354df
Kim-Jaemin420 <jany240@naver.com>
2021-07-04 02:06:47 +0900

    fix: 안쓰는 리듀서 삭제

--- src/redux/modules/index.jsx
+++ src/redux/modules/index.jsx
@@ -1,10 +1,4 @@
 import { combineReducers } from 'redux';
-import h2A from './h2A';
-import i from './i';
-import i1 from './i1';
-import p1 from './p1';
-import ox2 from './ox2';
-import h1h3h31 from './h1h3h31';
 import inspectionList from './inspectionList';
 import docsInput from "./docsInput";
 
@@ -12,12 +6,6 @@ import docsInput from "./docsInput";
 const rootReducer = combineReducers({
   docsInput,
   inspectionList,
-  h2A,
-  h1h3h31,
-  i,
-  i1,
-  p1,
-  ox2,
 });
 
 export default rootReducer;
