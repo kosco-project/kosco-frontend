b3f79723d8bd1e88b3e205a6f1878b9abea73887
Kim-Jaemin420 <jany240@naver.com>
2021-07-04 01:59:26 +0900

    fix: 필요없는 컴포넌트 삭제

--- src/components/doc/P1/P1TableRow.jsx
+++ src/components/doc/P1/P1TableRow.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import DeleteButton from '../common/DeleteButton';
+import { useDispatch, useSelector } from 'react-redux';
+import useChangeD1 from '../../../hooks/useChangeD1';
+import { deleteList } from '../../../redux/modules/docsInput';
+import DeleteButton from '../../common/DeleteButton';
 
-const P1TableRow = ({ id, onRemove, num, onChange }) => {
-  const { ProductType, Qty, Size, Perform } = useSelector(state => state.p1.D1[id]);
+const P1TableRow = ({ id, num }) => {
+  const dispatch = useDispatch();
+  const { ProductType, Qty, Size, Perform } = useSelector(state => state.docsInput.D1[id]);
 
-  const inputArg = ({ target }) => onChange({ target, id });
+  const onChange = useChangeD1();
+
 
   return (
     <tr>
@@ -13,21 +17,21 @@ const P1TableRow = ({ id, onRemove, num, onChange }) => {
         <div>{num}</div>
       </td>
       <td>
-        <input type='text' value={ProductType} onChange={inputArg} name="ProductType"/>
+        <input type='text' data-form='D1' data-key={id} value={ProductType} onChange={onChange} name="ProductType"/>
       </td>
       <td>
-        <input type='text' value={Qty} onChange={inputArg} name="Qty"/>
+        <input type='text' data-form='D1' data-key={id} value={Qty} onChange={onChange} name="Qty"/>
       </td>
       <td>
-        <input type='text' value={Size} onChange={inputArg} name="Size"/>
+        <input type='text' data-form='D1' data-key={id} value={Size} onChange={onChange} name="Size"/>
       </td>
       <td>
-        <select value={Perform} onChange={inputArg} name="Perform">
+        <select data-form='D1' data-key={id} value={Perform} onChange={onChange} name="Perform">
           <option value='GOOD'>GOOD</option>
           <option value='BAD'>BAD</option>
         </select>
       </td>
-      <td onClick={() => onRemove(id)}>
+      <td onClick={() => dispatch(deleteList({ form: 'D1', id }))}>
         <DeleteButton />
       </td>
     </tr>
