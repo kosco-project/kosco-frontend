94fdcd304bdca104b3677a6a28efdf70bbf7e7a0
Kim-Jaemin420 <jany240@naver.com>
2021-07-04 22:54:12 +0900

    fix: 날짜 선택 커스텀 훅으로 분리

--- src/components/inspection/InspectionCondition.jsx
+++ src/components/inspection/InspectionCondition.jsx
@@ -1,11 +1,10 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo, useEffect } from 'react';
 import styled from 'styled-components';
 import { DatePicker, Radio } from 'antd';
-import { useHistory, useLocation } from 'react-router-dom';
-import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import { getInspectionList } from '../../redux/modules/inspectionList';
 import useGetList from '../../hooks/useGetList';
+import useSearchCondition from '../../hooks/useSearchCondition';
 
 const { RangePicker } = DatePicker;
 
@@ -43,37 +42,21 @@ const InspectionForm = styled.form`
 
 const InspectionCondition = () => {
   const dispatch = useDispatch();
-  const history = useHistory();
-  
-  const [start, setStart] = useState(sessionStorage.getItem('startDate') ? moment(sessionStorage.getItem('startDate')) : moment().subtract(1, 'M'));
-  const [end, setEnd] = useState(sessionStorage.getItem('endDate') ? moment(sessionStorage.getItem('endDate')) : moment());
-  const [process, setProcess] = useState(1);
+  const { start, end, process, onChangeDate, onChangeProcess } = useSearchCondition();
   
   const list = useGetList(start, end, process);
-  
 
-  const onChangeD = (_, [start, end]) => {
-    setStart(moment(start));
-    setEnd(moment(end));
-    history.push(`/inspectionList/${start}/${end}/${process}`);
-    sessionStorage.setItem('startDate', start);
-    sessionStorage.setItem('endDate', end);
-  };
-
-  const onChangeProcess = ({ target }) => {
-    setProcess(target.value);
-    history.push(`/inspectionList/${start.format('YYYY-MM-DD')}/${end.format('YYYY-MM-DD')}/${process}`);
-  };
 
   useEffect(() => {
-    if (list) dispatch(getInspectionList(list))
+    if (list) dispatch(getInspectionList(list));
+    
   }, [dispatch, list]);
 
   return (
     <InspectionForm>
       <div className='inspection-input-box'>
         <label htmlFor='DateRange'>날짜 :</label>
-        <RangePicker size='large' onChange={onChangeD} value={[start, end]} format={'YYYY-MM-DD'} allowClear={false} />
+        <RangePicker size='large' onChange={onChangeDate} value={[start, end]} format={'YYYY-MM-DD'} allowClear={false} />
       </div>
       <div className='inspection-input-box'>
         <label htmlFor='DateRange'>진행 :</label>
